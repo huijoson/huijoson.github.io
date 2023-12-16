c582a6e310598c898978996be59b9d63825bbf5d
Lawrence <hlchimeilawrence@gmail.com>
2023-12-16 22:26:12 +0800

    [fixed] type her name in love.

--- script.js
+++ script.js
@@ -1,19 +1,21 @@
 document.addEventListener('DOMContentLoaded', function () {
     var canvas = document.getElementById('heartCanvas');
     var context = canvas.getContext('2d');
-
-    context.beginPath();
-    var x = 150; // x 坐標
-    var y = 150; // y 坐標
-    var radius = 100; // 圓的半徑
-    var startAngle = 0; // 開始點
-    var endAngle = Math.PI + (Math.PI * 0.8) / 2; // 結束點
-    var rotation = 0; // 旋轉角度
-
-    context.arc(x, y, radius, startAngle, endAngle, false);
-    context.arc(x + 190, y, radius, startAngle, endAngle, false);
-    context.lineTo(x + 95, y + 280);
-    context.closePath();
-    context.fillStyle = 'red';
-    context.fill();
+    
+    // 繪製愛心
+    function drawHeart() {
+        context.beginPath();
+        context.moveTo(150, 150);
+        context.bezierCurveTo(150, 130, 130, 115, 100, 115);
+        context.bezierCurveTo(50, 115, 50, 150, 50, 150);
+        context.bezierCurveTo(50, 180, 80, 205, 150, 250);
+        context.bezierCurveTo(220, 205, 250, 180, 250, 150);
+        context.bezierCurveTo(250, 150, 250, 115, 200, 115);
+        context.bezierCurveTo(175, 115, 150, 130, 150, 150);
+        context.closePath();
+        context.fillStyle = 'red';
+        context.fill();
+    }
+    
+    drawHeart();
 });
