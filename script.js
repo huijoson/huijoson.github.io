d9b354e8b90554b9464e16a3a4a6ca987b40720e
Lawrence <hlchimeilawrence@gmail.com>
2023-12-16 22:30:29 +0800

    [fixed]fix love

--- script.js
+++ script.js
@@ -1,21 +1,77 @@
 document.addEventListener('DOMContentLoaded', function () {
     var canvas = document.getElementById('heartCanvas');
-    var context = canvas.getContext('2d');
-    
-    // 繪製愛心
-    function drawHeart() {
-        context.beginPath();
-        context.moveTo(150, 150);
-        context.bezierCurveTo(150, 130, 130, 115, 100, 115);
-        context.bezierCurveTo(50, 115, 50, 150, 50, 150);
-        context.bezierCurveTo(50, 180, 80, 205, 150, 250);
-        context.bezierCurveTo(220, 205, 250, 180, 250, 150);
-        context.bezierCurveTo(250, 150, 250, 115, 200, 115);
-        context.bezierCurveTo(175, 115, 150, 130, 150, 150);
-        context.closePath();
-        context.fillStyle = 'red';
-        context.fill();
+    if (canvas.getContext) {
+        var ctx = canvas.getContext('2d');
+        
+        var offsetX = canvas.width / 2;
+        var offsetY = canvas.height / 2 - 20;
+        var scale = 10; // Change the size of the heart here
+
+        // Heart parameters
+        var topCurveHeight = scale * 3.5;
+        var verticalHeartHeight = scale * 2.5;
+        var horizontalHeartWidth = scale * 2.5;
+        var bottomCurveHeight = scale * 2.5;
+
+        ctx.beginPath();
+        // Start at bottom of heart
+        ctx.moveTo(offsetX, offsetY + verticalHeartHeight);
+        
+        // Draw top left curve
+        ctx.bezierCurveTo(
+            offsetX, offsetY + verticalHeartHeight - topCurveHeight,
+            offsetX - horizontalHeartWidth, offsetY + verticalHeartHeight - topCurveHeight,
+            offsetX - horizontalHeartWidth, offsetY
+        );
+
+        // Draw bottom left curve
+        ctx.bezierCurveTo(
+            offsetX - horizontalHeartWidth, offsetY - bottomCurveHeight,
+            offsetX, offsetY - bottomCurveHeight - topCurveHeight,
+            offsetX, offsetY
+        );
+
+        // Draw bottom right curve
+        ctx.bezierCurveTo(
+            offsetX, offsetY - bottomCurveHeight - topCurveHeight,
+            offsetX + horizontalHeartWidth, offsetY - bottomCurveHeight,
+            offsetX + horizontalHeartWidth, offsetY
+        );
+
+        // Draw top right curve
+        ctx.bezierCurveTo(
+            offsetX + horizontalHeartWidth, offsetY + verticalHeartHeight - topCurveHeight,
+            offsetX, offsetY + verticalHeartHeight - topCurveHeight,
+            offsetX, offsetY + verticalHeartHeight
+        );
+        
+        ctx.fillStyle = 'red';
+        ctx.fill();
+        ctx.closePath();
+
+        // Add animated message
+        var message = "詠萱2024依然愛妳";
+        var messageOffsetY = offsetY + scale * 6;
+        ctx.font = '22px Arial';
+        ctx.fillStyle = 'black';
+        ctx.textAlign = 'center';
+
+        // Animate the message
+        var opacity = 1;
+        var fadeIn = true;
+        setInterval(function() {
+            ctx.clearRect(0, messageOffsetY - 30, canvas.width, 100); // Clear the old text
+            ctx.globalAlpha = opacity;
+            ctx.fillText(message, offsetX, messageOffsetY); // Draw new text
+
+            // Update the opacity for fade in/out effect
+            if (fadeIn) {
+                opacity -= 0.01;
+                if (opacity <= 0) fadeIn = false;
+            } else {
+                opacity += 0.01;
+                if (opacity >= 1) fadeIn = true;
+            }
+        }, 50);
     }
-    
-    drawHeart();
 });
