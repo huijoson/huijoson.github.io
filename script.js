f5387056fcf41fe4b49e5c20f496043926f1edc2
yuhan <yuhanlai@btch.org.tw>
2024-12-17 09:13:39 +0800

    added qrcode

--- script.js
+++ script.js
@@ -24,4 +24,15 @@ window.addEventListener('load', () => {
     setTimeout(() => {
         photo.style.opacity = 1;
     }, 500);
+});
+
+window.addEventListener('load', function() {
+    const qrcode = new QRCode(document.getElementById("qrcode"), {
+        text: "https://huijoson.github.io/",
+        width: 128,
+        height: 128,
+        colorDark : "#000000",
+        colorLight : "#ffffff",
+        correctLevel : QRCode.CorrectLevel.H
+    });
 }); 
